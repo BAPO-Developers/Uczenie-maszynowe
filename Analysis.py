917785e93edaeab04b333c2437d7a7d581a3339b
bartek3012 <62774530+bartek3012@users.noreply.github.com>
2021-05-24 10:36:59 +0200

    Small refactor
    Small refactor

--- Analysis.py
+++ Analysis.py
@@ -7,11 +7,13 @@ accuracy = np.load(r'Results\accuracy.npy')  # 3-wymiarowa tabela z wartościami
 data_files_names = np.load(r'Results\data_files_names.npy')  # Nazwy użytych baz danych
 clf_names = np.load('Results\clf_names.npy')  # Nazwy klasyfikatorów bazowych
 wilcoxon_test = Statistic.wilcoxon(clf_names.tolist(), accuracy)  # 2-wymiarowa tablica z testem Wilcoxona
+
+# Wykonanie testu t-Studenta dla każego zbioru danych, z posiomem ufności 0.05
 t_student_test = []
 for i, file_name in enumerate(data_files_names):
-    t_student_test.append(Statistic.t_student(clf_names.tolist(), accuracy[:, i, :]))
+    t_student_test.append(Statistic.t_student(clf_names.tolist(), accuracy[:, i, :], .05))
 
-
+# Tworzenie tabel w formace LaTeX
 def prepare_latex_data():
     for t_s in t_student_test:
         sign_t = [""]
--- Statistic.py
+++ Statistic.py
@@ -3,7 +3,7 @@ from scipy.stats import ttest_ind, rankdata, ranksums
 from tabulate import tabulate
 
 
-def t_student(headers_array, scores, alfa=.05, print_result=False):
+def t_student(headers_array, scores, alfa=.05):
     t_statistic = np.zeros((len(headers_array), len(headers_array)))
     p_value = np.zeros((len(headers_array), len(headers_array)))
 
@@ -22,35 +22,10 @@ def t_student(headers_array, scores, alfa=.05, print_result=False):
 
     # Wymnożenie macieży przewag i macieży znaczności
     stat_better = significance * advantage
-    if print_result == True:
-        # Printowanie danych
-        headers_array_in_array = []  # Tabela z nazwami w formacie np [["GNB"], ["kNN"], ["CART"]]
-        for name in headers_array:
-            temp = []
-            temp.append(name)
-            headers_array_in_array.append(temp)
-
-        t_statistic_table = np.concatenate((headers_array_in_array, t_statistic), axis=1)
-        t_statistic_table = tabulate(t_statistic_table, headers_array, floatfmt=".2f")
-
-        p_value_table = np.concatenate((headers_array_in_array, p_value), axis=1)
-        p_value_table = tabulate(p_value_table, headers_array, floatfmt=".2f")
-
-        advantage_table = tabulate(np.concatenate((headers_array_in_array, advantage), axis=1), headers_array)
-
-        significance_table = tabulate(np.concatenate((headers_array_in_array, significance), axis=1), headers_array)
-
-        stat_better_table = tabulate(np.concatenate((headers_array_in_array, stat_better), axis=1), headers_array)
-
-        print('------------------------------------------------------------------')
-        print(
-            f"t-statistic:\n {t_statistic_table} \n\np-value:\n {p_value_table} \n\nAdvantage:\n {advantage_table} \n\nStatistical "
-            f"significance (alpha = {alfa}):\n {significance_table} \n\nStatistically significantly better:\n {stat_better_table}")
-        print('------------------------------------------------------------------')
     return stat_better
 
 
-def wilcoxon(headers_array, scores, alpha=.05, print_result=False):
+def wilcoxon(headers_array, scores, alpha=.05):
     # Średnie wyniki dla każdego z foldów
     mean_scores = np.mean(scores, axis=2).T
     # Przypisanie rang od 1 do (liczby estymatorów) w przypadku remisów uśredniamy
@@ -75,12 +50,4 @@ def wilcoxon(headers_array, scores, alpha=.05, print_result=False):
 
     # Wymnożenie macieży przewag i macieży znaczności
     stat_better = significance * advantage
-    if print_result == True:
-        headers_array_in_array = np.expand_dims(np.array(headers_array), axis=1)
-        stat_better_table = tabulate(np.concatenate((headers_array_in_array, stat_better), axis=1), headers_array)
-        advantage_table = tabulate(np.concatenate((headers_array_in_array, advantage), axis=1), headers_array)
-        significance_table = tabulate(np.concatenate((headers_array_in_array, significance), axis=1), headers_array)
-        print("\nAdvantage:\n", advantage_table)
-        print(f"\nStatistical significance (alpha = {alpha}):\n{significance_table}")
-        print("\nStatistically significantly better::\n", stat_better_table)
     return stat_better
